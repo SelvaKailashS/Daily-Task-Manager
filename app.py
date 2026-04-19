5b74e9a2a39571a758e4d8946b892a8be9c70d38
Selva Kailash S <salujaradha9@gmail.com>
2026-04-19 20:22:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -323,5 +323,9 @@ def profile():
 
 if __name__ == '__main__':
     with app.app_context():
+    db.create_all()
+
+if __name__ == '__main__':
+    with app.app_context():
         db.create_all()
     app.run(debug=True)
